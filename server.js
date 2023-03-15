7cd7d76d7927b9bab1dffd184fb807c50ba4be2f
DocRitik <ritik.aggarwal@docquity.com>
2023-03-16 02:12:39 +0530

    resolve port error for deployment

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 // Start the server and initialize socket.io
 
-const server = app.listen(process.env.port, process.env.url);
+const server = app.listen(process.env.PORT, () => console.log(`Listening at `, process.env.PORT));
 const io = require("socket.io")(server);
 app.use(express.json())
 
